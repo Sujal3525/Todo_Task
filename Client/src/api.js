c164012ffc327ba31e2e27204e82b2bed25de81d
Suja3525 <mr.sujal.3525@gmail.com>
2026-04-14 17:26:27 +0530

    Connecting frontend to live render backend

--- Client/src/api.js
+++ Client/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:8080/api' });
+const API = axios.create({ baseURL: 'https://todo-task-ztj8.onrender.com/api' });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem('token');
