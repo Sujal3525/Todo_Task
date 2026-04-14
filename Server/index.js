b622e1005f70176cbb082aefa8a06abd03cd70de
Suja3525 <mr.sujal.3525@gmail.com>
2026-04-14 17:38:09 +0530

    Final Fix: Consolidated CORS and route order

--- Server/index.js
+++ Server/index.js
@@ -6,17 +6,16 @@ const app = express();
 
 connectDB();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://todo-task-nine-psi.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
 app.use(express.json());
 
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/tasks', require('./routes/taskRoutes'));
-
-app.use(cors({
-  origin: ["https://todo-task-nine-psi.vercel.app/"],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
 
 app.get("/", (req, res) => res.send("API Running"));
 
