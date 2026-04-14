5ab2ba030b5eccb4261ddaf45e2407bd1aacb2d7
Suja3525 <mr.sujal.3525@gmail.com>
2026-04-14 17:33:50 +0530

    correcting cors error for vercel

--- Server/index.js
+++ Server/index.js
@@ -22,4 +22,3 @@ app.get("/", (req, res) => res.send("API Running"));
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-https://todo-task-nine-psi.vercel.app/
