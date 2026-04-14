013acd09d7203950b0e2ecfea71d70e9721c150d
Suja3525 <mr.sujal.3525@gmail.com>
2026-04-14 17:33:25 +0530

    correcting cors error

--- Server/index.js
+++ Server/index.js
@@ -12,7 +12,14 @@ app.use(express.json());
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/tasks', require('./routes/taskRoutes'));
 
+app.use(cors({
+  origin: ["https://todo-task-nine-psi.vercel.app/"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
 app.get("/", (req, res) => res.send("API Running"));
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+https://todo-task-nine-psi.vercel.app/
